0188dd747782b88b771af7af62145b8c33977ae1
unknown <04212@ASUS-21298.libertytimes.com.tw>
2015-11-09 14:45:36 +0800

    test1

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
-<?php
-echo "hello word";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>會員登入</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
+</head>
+<body>
+<style type="text/css">
+
+html,body{	
+	height: 80%;
+	background-color: #A1FFFF;
+}
+#container {
+	position: relative;
+    height: 80%;
+}
+#header {
+	background-color:#A1FFFF;
+	text-align: center;height: 20%;	
+}
+.content {
+	position: relative;
+	/*background-color:#EEEEEE;*/
+	height:80%;width:100%;
+	float:left;
+	margin-top: 0.5cm;
+	margin-bottom: 1cm;
+}
+</style>
+<div id = "header">
+	<h1>會員登入</h1>
+</div>
+<div id = "container">
+	<div class ="content" align ="center">
+	<form name ="form" method ="post" action ="check.php">
+		<h3>帳號</h3><input type ="text" name ="account" /> <br>
+		<h3>密碼</h3><input type ="password" name ="password" /> <br>
+		<br>
+		<input type ="submit" name ="button" value ="登入" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href ="register.php">申請帳號</a>
+	</form>
+	</div>
+</div>
+
+</body>
+</html>
